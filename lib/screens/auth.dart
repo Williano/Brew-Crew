50990aa2a85c3403b39ea0b96325669ca7aafcf9
Williano <paawilly17@gmail.com>
2020-02-24 21:26:08 -0600

    Add functionality for signing in anonnymously

---
+++ lib/screens/auth.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anonnymous
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    }
+    catch(e) {
+
+      print(e.toString());
+      return null;
+
+    }
+  }
+
+  // sign in with email & password
+
+  // register with eamil & password
+
+  // sign out
+}
