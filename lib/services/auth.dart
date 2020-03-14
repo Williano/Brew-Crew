9bd13037d98880ebe45eaa8caef6e0314f74385f
Williano <paawilly17@gmail.com>
2020-03-13 22:15:23 -0500

    Add user collection creation

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
@@ -54,6 +55,10 @@ class AuthService {
 
       AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       FirebaseUser user= result.user;
+
+      // create a new document for the user with the uid
+      await DatabaseService(uid: user.uid).updateUserData("0", "New crew member", 100);
+
       return _userFromFirebaseUser(user);
 
     } catch(e){
